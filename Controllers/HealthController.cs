e866a68315ae927acff38f8be49f5c4a3accfb7d
Louis-Olivier Major <louis.olivier.major@gmail.com>
2026-01-31 11:03:40 -0500

    Healh controller to test Azure

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhizSheet.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/health")]
+public class HealthController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult Get()
+	{
+		return Ok(new
+		{
+			status = "ok",
+			environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "unknown"
+		});
+	}
+}
